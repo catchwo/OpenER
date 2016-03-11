4690a5f4c0061125732221b08588a74a2ce36bb2
catchwo <catchwo@hotmail.com>
2016-03-11 14:11:49 +0800

    r

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,45 @@ import java.*;
 
 public class HelloWorld
 {
+    public  static  Thread M;
     public  static  void  main(String args[])
     {
-        System.out.println("safdasfafdsfDS");
+        M = Thread.currentThread();
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true)
+                {
+                    //Thread.sleep(1000);
+                    try
+                    {
+                            System.out.println("i am running..........sdf色污染...");
+                            Thread.sleep(1000);
+                            System.out.println("m state is .........."+M.getState().toString());
+                    }
+                    catch ( java.lang.Exception e){ //java.lang.InterruptedException e){
+                        //Thread.currentThread().isInterrupted();
+                        //Thread.currentThread().interrupt();
+                        //Thread.interrupted();
+                        //System.out.println("i am"+Thread.currentThread().isInterrupted());
+                    }
+                }
+            }
+        }) ;
+        t.start();
+        t.interrupt();
+        System.out.println("t Interrupted is "+t.isInterrupted());
+        System.out.println("safdasfafdsfDS"+Thread.currentThread().getId());
+
+
+        try
+        {
+            t.join(5000);
+        }
+        catch ( java.lang.InterruptedException e){
+            System.out.println("i am"+Thread.currentThread().isInterrupted());
+    }
+
     }
 }
